d21870698f80858f8f4a9022d187fa43739badee
AaronR <aaron@afterrabble.com>
2016-10-30 00:12:20 -0700

    cleaning up for submission

--- imageProcessing/ViewController.swift
+++ imageProcessing/ViewController.swift
@@ -12,91 +12,43 @@ import AssetsLibrary
 
 class ViewController: UIViewController {
 
-    
-
-    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var imageView: UIImageView!
     
-    @IBAction func saveImage(_ sender: AnyObject) {
-        
-        UIImageWriteToSavedPhotosAlbum(imageView.image!, self,#selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
-        
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //logAllFilters()
         //let image = UIImage(named: "dark1.jpg")!
         var newImage: UIImage? = nil
-        let start = 4562
-        let finish = 4594
+        let start = 0
+        let finish = 30
         
         for index1 in start...finish{
             
             let image = (UIImage(named:"IMG_\(index1).JPG")!)
+            //if first run through loop newImage will be nil
             if newImage == nil{
                 newImage = image
             }
+            // all other times do normal stuff
             else{
                 newImage = overLay2(image1: newImage!, image2: image).toUIImage()!
             }
-            print(index1)
-            
-            
+            print(index1) // prints current image index
          
         }
         
-        
-        
-        let filter = CIFilter(name: "CIMedianFilter")
-        filter?.setValue(CIImage(image: newImage!), forKey: kCIInputImageKey)
-        //filter?.setValue(0.5, forKey: kCIInputIntensityKey)
-        
-        let outImage = filter?.outputImage
-        
-        
-        //set sned image to UIImageView on divice
-        //imageView.image = UIImage(ciImage: outImage!)
-        //imageView.contentMode = UIViewContentMode.scaleAspectFit
         imageView.image = newImage
         
         
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    func logAllFilters() {
-        let properties = CIFilter.filterNames(inCategory: kCICategoryBuiltIn)
-        print(properties)
-        
-        for filterName in properties {
-            let fltr = CIFilter(name:filterName as String)
-            print(fltr!.attributes)
-        }
-    }
 
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        
-        print("There is a memory warning _*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*")
-        // Dispose of any resources that can be recreated.
-    }
-    
-    func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
-        if let error = error {
-            // we got back an error!
-            
-            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
-            ac.addAction(UIAlertAction(title: "Figure out Why", style: .default))
-            present(ac, animated: true)
-        } else {
-            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
-            ac.addAction(UIAlertAction(title: "OK", style: .default))
-            present(ac, animated: true)
-        }
-    }
-    
 
+    }
 
 }
-
